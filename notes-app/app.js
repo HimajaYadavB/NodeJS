3cfc2bbd9be10029fc92e52f7d68ee1648a3c052
Himaja B <himajayadav1369@gmail.com>
2025-12-23 16:48:33 +0530

    initial commit

---
+++ notes-app/app.js
@@ -0,0 +1,20 @@
+const notes = require('./notes.js')
+console.log(notes());
+
+
+
+
+// const fs = require('fs')
+
+// fs.writeFileSync('notes.txt','My name is Himaja.')  //text will be overwritten
+
+// //challenge: append message to notes.txt using appendFileSync
+
+// fs.appendFileSync('notes.txt',' Appended text')
+
+// const utils = require('./utils.js')
+
+// console.log(utils.name)
+// console.log(utils.add('4','5'));
+// console.log(utils.add(4,5));
+
